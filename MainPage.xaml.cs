5a79481baef356bd411071f99381f05c86a69b63
sgaona21 <88602650+sgaona21@users.noreply.github.com>
2025-03-31 15:05:50 -0700

    project setup

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -26,5 +26,10 @@ namespace AlbumCoverMatchGame
         {
             this.InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
